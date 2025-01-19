4a320dab28cfdb73bf1659bbd6ec3162fe4cf560
JohnDominicJasmin <johndominicjasmin@gmail.com>
2025-01-19 09:17:15 +0800

    fix appregistry issue

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import { Platform } from 'react-native';
+import { Platform,AppRegistry } from 'react-native';
 import App from './App';
 import { initFirebase } from './firebaseconfig';
 import registerRootComponent from 'expo/build/launch/registerRootComponent';
 
 // Initialize Firebase
 initFirebase();
-
+AppRegistry.registerComponent("X", () => App);
 // Register the root component
 registerRootComponent(App);
